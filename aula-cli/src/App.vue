b8ef9e9e9b14e6402f3b38ace16879a43aa264da
isatluizlopes <luizlopes12@outlook.com>
2022-06-15 12:02:37 -0300

    Primeiros componentes

---
+++ aula-cli/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <primeiroComponente/>
+</template>
+<script>
+import primeiroComponente from './components/primeiroComponente.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    primeiroComponente
+  }
+}
+</script>
---
+++ aula-cli/src/components/primeiroComponente.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Olá Vue!</h1>
+</template>
+
+
+<script>
+export default {
+    name: 'primeiroComponente'
+}
+</script>
